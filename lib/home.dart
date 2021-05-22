0ad8eb2cbc002af39b4f8cba81db8f2de1cc8759
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-22 12:34:14 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,12 @@ import './screens/note_list.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 import 'dart:io';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
+
 import 'package:path_provider/path_provider.dart';
 import 'package:note_keeper_app/models/note.dart';
 import 'package:intl/intl.dart';
