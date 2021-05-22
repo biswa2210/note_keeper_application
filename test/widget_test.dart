f50064cebe8a98de40d8584656f9dec712860472
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-22 12:34:56 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,11 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
